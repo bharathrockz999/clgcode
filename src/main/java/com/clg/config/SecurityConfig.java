c3be9f2c872b4215cbf2ed8bd62b30c87d4a1235
S556370 <s556370@nwmissouri.edu>
2023-11-26 22:10:33 -0600

    Update SecurityConfig

--- src/main/java/com/clg/config/SecurityConfig.java
+++ src/main/java/com/clg/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
-//                .cors(cors -> cors.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests()
                 .requestMatchers("/user/authenticate","/blog/page/get","/blog/page/get/category","/user/new","/user/sendCode","/user/forgotPassword","/user/changePassword/**","/blog/page/get/category").permitAll()
                 .and()
